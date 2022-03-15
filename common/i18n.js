50750223ae322e7f22915f0bdd5cc19117929cb5
Sonia Ruiz <yune.vk@gmail.com>
2022-03-15 09:41:08 +0100

    🚧  Set content contract and add pages

---
+++ common/i18n.js
@@ -0,0 +1,4 @@
+import literals from "../content/commons.json"
+
+// TODO: rename function
+export const t = (key) => literals[key]
---
+++ content/home/5-connection.md
@@ -0,0 +1,5 @@
+---
+anchorNavSection: "Connection"
+title: "Stay up to date with events and updates"
+button: "Repository"
+---
