37b1f4579aef4c2410e42205bdb9894eef52c530
Sonia Ruiz <yune.vk@gmail.com>
2022-03-16 10:42:03 +0100

    💄 Header and footer base styles

---
+++ ui/components/header/Header.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+
+import { getRelativeURL } from '../../../common/routes'
+import { getLiteral } from '../../../common/i18n'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <Link href={getRelativeURL('/')}>
+        <a className="header__logo">{`${getLiteral('page:title')} ${getLiteral(
+          'page:date',
+        )}`}</a>
+      </Link>
+
+      <nav className="header__navigation">
+        <ul className="header__list">
+          <li>
+            <Link href={getRelativeURL('/schedule')}>
+              {getLiteral('navigation:schedule')}
+            </Link>
+          </li>
+          <li>
+            <Link href={getRelativeURL('/resources')}>
+              {getLiteral('navigation:resources')}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- ui/layout/MainLayout.jsx
+++ ui/layout/MainLayout.jsx
@@ -1,64 +1,12 @@
-import Link from 'next/link'
-
-import { getRelativeURL } from '../../common/routes'
-import { getLiteral } from '../../common/i18n'
+import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header'
 
 const MainLayout = ({ children }) => {
   return (
     <>
-      <Link href={getRelativeURL('/')}>{`${getLiteral(
-        'page:title',
-      )} ${getLiteral('page:date')}`}</Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href={getRelativeURL('/schedule')}>
-              {getLiteral('navigation:schedule')}
-            </Link>
-          </li>
-          <li>
-            <Link href={getRelativeURL('/resources')}>
-              {getLiteral('navigation:resources')}
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Header />
       <main>{children}</main>
-      <footer>
-        <p>{getLiteral('footer:copyright')}</p>
-
-        <a
-          href={getLiteral('footer:terms-url')}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:terms-title')}
-        </a>
-        <a
-          href={getLiteral('footer:privacy-url')}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:privacy-title')}
-        </a>
-        <a
-          href={`mailto:${getLiteral('footer:email')}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:email')}
-        </a>
-        <a
-          href={getLiteral('footer:repository-url')}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:repository-title')}
-        </a>
-        <a href={getLiteral('footer:coc-url')} target="_blank" rel="noreferrer">
-          {getLiteral('footer:coc-title')}
-        </a>
-      </footer>
+      <Footer />
     </>
   )
 }
