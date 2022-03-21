a23fdd99a7b504d1f6222e89d137ed3f47e10048
Sonia Ruiz <yune.vk@gmail.com>
2022-03-21 14:45:35 +0100

    🚧 💄 WIP Get involved

--- components/home/get-involved/GetInvolved.jsx
+++ components/home/get-involved/GetInvolved.jsx
@@ -1,16 +1,32 @@
-const GetInvolved = ({ title, examplesTitle, examples }) => {
+import md from 'markdown-it'
+
+const GetInvolved = ({ title, examplesTitle, examples, content }) => {
   return (
     <section className="get-involved">
-      <h2>{title}</h2>
+      <div className="get-involved__content">
+        <div className="get-involved__header">
+          <h2 className="get-involved__title">{title}</h2>
 
-      <h3>{examplesTitle}</h3>
-      <div className="get-involved__examples">
-        {examples.map(({ title, image }) => (
-          <div key={`example-${title}`} className="get-involved__example">
-            <p>{title}</p>
-            {/* TODO: image */}
+          <div
+            className="get-involved__text"
+            dangerouslySetInnerHTML={{ __html: md().render(content) }}
+          />
+        </div>
+
+        <div className="get-involved__examples">
+          <h3 className="get-involved__examples-title">{examplesTitle}</h3>
+
+          <div className="get-involved__list">
+            <div className="get-involved__list-content">
+              {examples.map(({ title, image }) => (
+                <div key={`example-${title}`} className="get-involved__example">
+                  <p>{title}</p>
+                  {/* TODO: image */}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
       </div>
     </section>
   )
