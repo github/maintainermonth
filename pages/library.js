33f650a1e4b25c99cc272ef31a6f008da2a08c89
Sonia Ruiz <yune.vk@gmail.com>
2022-03-22 08:33:03 +0100

    🚚  Rename resources page to library

--- pages/library.js
+++ pages/library.js
@@ -2,24 +2,21 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 import { getLiteral } from '../common/i18n'
-import resourcesJSON from '../content/resources/resources.json'
+import resourcesJSON from '../content/library/resources.json'
 
 const { resources } = resourcesJSON
 
-export default function Resources() {
+export default function Library() {
   return (
     <div>
       <Head>
-        <title>{getLiteral('resources:title')}</title>
-        <meta
-          name="description"
-          content={getLiteral('resources:description')}
-        />
+        <title>{getLiteral('library:title')}</title>
+        <meta name="description" content={getLiteral('library:description')} />
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main>
-        <h1>{getLiteral('resources:title')}</h1>
+        <h1>{getLiteral('library:title')}</h1>
 
         {resources.map((resource) => (
           <div key={resource.title}>
