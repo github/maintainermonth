8cfc3a21bd7cf752d174be2c9d7c91ca18332f65
Sonia Ruiz <yune.vk@gmail.com>
2022-03-29 13:10:26 +0200

    🚧  WIP generate og images

---
+++ components/og-image/getOgImage.js
@@ -0,0 +1,29 @@
+const { createHash } = require('crypto')
+
+var Jimp = require('jimp')
+
+async function getOgImage(title) {
+  //   if (process.env.NODE_ENV === 'development') {
+  //     return 'og image will be generated in production'
+  //   }
+
+  const hash = createHash('md5').update(title).digest('hex')
+
+  Jimp.read('public/images/og/templates/event.png', (err, image) => {
+    if (err) throw err
+
+    Jimp.loadFont('public/fonts/inter/inter.fnt')
+      .then((font) => {
+        image
+          .print(font, 100, 100, title, 900)
+          .write(`public/images/og/event-${hash}.png`)
+      })
+      .catch((err) => {
+        console.log('Error loading font', err)
+      })
+  })
+
+  return `/images/og/event-${hash}.png`
+}
+
+export default getOgImage
