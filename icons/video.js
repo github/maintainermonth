fd27c0e488f3ccf0bc70f0f6f033aad75296116b
YuneVK <yune.vk@gmail.com>
2022-04-06 12:31:47 +0000

    Deploying to gh-pages from @ github/maintainermonth@508b638eb25528b8633429a330caaefcad66d107 🚀

---
+++ icons/video.js
@@ -0,0 +1,21 @@
+const IconVideo = () => {
+  return (
+    <svg
+      className="icon-video"
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M16.5 10.5L6.5 17.5L6.5 3.5L16.5 10.5Z" fill="black" />
+      <path
+        d="M16.5 10.5L6.5 17.5L6.5 3.5L16.5 10.5Z"
+        fill="black"
+        fillOpacity="0.2"
+      />
+    </svg>
+  )
+}
+
+export default IconVideo
