2a62c47193d4baff11f7584929dcdd22e753b9b7
Sonia Ruiz <yune.vk@gmail.com>
2022-04-06 18:37:57 +0200

    👷 Add mock images

--- components/home/hero/Hero.jsx
+++ components/home/hero/Hero.jsx
@@ -5,6 +5,7 @@ import * as ROUTES from '../../../common/routes'
 const Hero = ({ date, title, buttonText }) => {
   return (
     <section className="hero">
+      <div className="hero__background" />
       <div className="hero__content">
         <p className="hero__chip">{date}</p>
         <h1 className="hero__title">{title}</h1>
