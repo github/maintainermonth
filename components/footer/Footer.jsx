9605e41ce1b6feac2f41579bfbb1ac7ec56afc69
Sonia Ruiz <yune.vk@gmail.com>
2022-04-11 17:15:50 +0200

    🩹 💄 Fix index issues

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,4 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
+import { useMemo } from 'react'
+import { useRouter } from 'next/router'
+
+import * as ROUTES from '../../common/routes'
+
 import { getLiteral } from '../../common/i18n'
 
 import IconTwitter from '../../public/icons/twitter'
@@ -8,99 +13,107 @@ import IconLinkedIn from '../../public/icons/linkedin'
 import IconGitHub from '../../public/icons/github'
 
 const Footer = () => {
+  const { pathname } = useRouter()
+
+  const isHome = useMemo(() => pathname === ROUTES.HOME.path, [pathname])
+
   return (
-    <footer className="footer">
-      <div className="footer__background">
-        <img
-          className="footer__image"
-          src="/images/footer.jpg"
-          alt={getLiteral('footer:image-description')}
-        />
-      </div>
+    <>
+      <footer className="footer">
+        <div className="footer__copyright">
+          <a
+            className="footer__link"
+            href={getLiteral('footer:repository-url')}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {getLiteral('footer:repository-title')}
+          </a>
+          <span className="footer__divider" />
 
-      <div className="footer__copyright">
-        <a
-          className="footer__link"
-          href={getLiteral('footer:repository-url')}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:repository-title')}
-        </a>
-        <span className="footer__divider" />
-
-        <p>{getLiteral('footer:copyright')}</p>
-      </div>
-
-      <div className="footer__links">
-        <div className="footer__social">
-          <a
-            className="footer__button"
-            href={getLiteral('footer:twitter-url')}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={getLiteral('footer:twitter-label')}
-          >
-            <span aria-hidden="true">
-              <IconTwitter />
-            </span>
-          </a>
-          <a
-            className="footer__button"
-            href={getLiteral('footer:facebook-url')}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={getLiteral('footer:facebook-label')}
-          >
-            <span aria-hidden="true">
-              <IconFacebook />
-            </span>
-          </a>
-          <a
-            className="footer__button"
-            href={getLiteral('footer:youtube-url')}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={getLiteral('footer:youtube-label')}
-          >
-            <span aria-hidden="true">
-              <IconYouTube />
-            </span>
-          </a>
-          <a
-            className="footer__button"
-            href={getLiteral('footer:linkedin-url')}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={getLiteral('footer:linkedin-label')}
-          >
-            <span aria-hidden="true">
-              <IconLinkedIn />
-            </span>
-          </a>
-          <a
-            className="footer__button"
-            href={getLiteral('footer:github-url')}
-            target="_blank"
-            rel="noreferrer"
-            aria-label={getLiteral('footer:github-label')}
-          >
-            <span aria-hidden="true">
-              <IconGitHub />
-            </span>
-          </a>
+          <p>{getLiteral('footer:copyright')}</p>
         </div>
 
-        <a
-          className="footer__link"
-          href={getLiteral('footer:privacy-url')}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {getLiteral('footer:privacy-title')}
-        </a>
-      </div>
-    </footer>
+        <div className="footer__links">
+          <div className="footer__social">
+            <a
+              className="footer__button"
+              href={getLiteral('footer:twitter-url')}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={getLiteral('footer:twitter-label')}
+            >
+              <span aria-hidden="true">
+                <IconTwitter />
+              </span>
+            </a>
+            <a
+              className="footer__button"
+              href={getLiteral('footer:facebook-url')}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={getLiteral('footer:facebook-label')}
+            >
+              <span aria-hidden="true">
+                <IconFacebook />
+              </span>
+            </a>
+            <a
+              className="footer__button"
+              href={getLiteral('footer:youtube-url')}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={getLiteral('footer:youtube-label')}
+            >
+              <span aria-hidden="true">
+                <IconYouTube />
+              </span>
+            </a>
+            <a
+              className="footer__button"
+              href={getLiteral('footer:linkedin-url')}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={getLiteral('footer:linkedin-label')}
+            >
+              <span aria-hidden="true">
+                <IconLinkedIn />
+              </span>
+            </a>
+            <a
+              className="footer__button"
+              href={getLiteral('footer:github-url')}
+              target="_blank"
+              rel="noreferrer"
+              aria-label={getLiteral('footer:github-label')}
+            >
+              <span aria-hidden="true">
+                <IconGitHub />
+              </span>
+            </a>
+          </div>
+
+          <a
+            className="footer__link"
+            href={getLiteral('footer:privacy-url')}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {getLiteral('footer:privacy-title')}
+          </a>
+        </div>
+      </footer>
+
+      {isHome ? (
+        <div className="footer__background">
+          <img
+            className="footer__image"
+            src="/images/footer.jpg"
+            alt={getLiteral('footer:image-description')}
+          />
+        </div>
+      ) : null}
+    </>
   )
 }
 
