98f9680b4c45b5f4eefb836c5459328553ca9eb1
Sonia Ruiz <yune.vk@gmail.com>
2022-04-13 11:21:23 +0200

    🔍  Set 404 page meta tags

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,40 @@
+import Head from 'next/head'
+
+import { getLiteral } from '../common/i18n'
 import NotFound from '../components/not-found/NotFound'
 
 export default function NotFoundPage() {
-  return <NotFound />
+  return (
+    <div>
+      <Head>
+        <title>{getLiteral('meta:title')}</title>
+        <meta name="description" content={getLiteral('meta:description')} />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:title" content={getLiteral('meta:title')} />
+        <meta
+          property="og:description"
+          content={getLiteral('meta:description')}
+        />
+        <meta
+          property="og:image"
+          content="https://maintainermonth.github.com/images/og/generic.png"
+        />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={getLiteral('meta:title')} />
+        <meta
+          name="twitter:description"
+          content={getLiteral('meta:description')}
+        />
+        <meta
+          name="twitter:image"
+          content="https://maintainermonth.github.com/images/og/generic.png"
+        />
+      </Head>
+
+      <NotFound />
+    </div>
+  )
 }
