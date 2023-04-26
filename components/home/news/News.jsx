431fd0195593fa08d1810d059e1e63c57e06c2c0
Abhinav Rajesh <abhinavrajesh@github.com>
2023-04-26 20:43:25 +0000

    feat: added news section in home page

---
+++ components/home/news/News.jsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+
+import SectionDivider from "../../section-divider/SectionDivider"
+import ButtonLink from "../../button-link/ButtonLink";
+
+import * as ROUTES from "../../../common/routes"
+
+const News = ({ title, news }) => {
+    const { pathname } = useRouter()
+    const year = Number(pathname.split('/')[1]) > 404 ? Number(pathname.split('/')[1]) : null
+
+    
+    return (
+        <section className="news">
+            <div className="news__content">
+                <SectionDivider title={title} />
+                <div className="news__items">
+                    {news.map(({ title: newsTitle, description, link }, key) => (
+                        <div className="news__item" key={key}>
+                            <a href={link} className="news__link">
+                                <h4 className="news__text">{newsTitle}</h4>
+                            </a>
+                            <p className="news__text">{description}</p>
+                        </div>
+                    ))}
+                    <div className="news__item news__button">
+                        <ButtonLink children={"View all announcements"} href={ROUTES.NEWS.getPath(year)} />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default News;
