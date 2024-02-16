e7696b76520f2d2d8d5ef11e730d0063a4357b4c
Lee Reilly <lee@github.com>
2024-02-15 18:56:22 -0800

    Lee in classic off-by-one error

--- components/home/council/Council.jsx
+++ components/home/council/Council.jsx
@@ -12,7 +12,7 @@ const Council = () => {
     <section className="about">
       <div className="about__content">
         <div className="about__column">
-          <h2 className="about__title">Maintainer Month 2023 Advisory Council</h2>
+          <h2 className="about__title">Maintainer Month 2024 Advisory Council</h2>
         </div>
 
         <div className="about__text">
