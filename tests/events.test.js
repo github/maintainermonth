e0c82ccdb9c51c586ab6ba33daa8f297c952f501
Lee Reilly <lee@github.com>
2024-02-16 16:44:55 +0000

    Reset events tests

--- tests/events.test.js
+++ tests/events.test.js
@@ -6,28 +6,22 @@ describe('Event files', () => {
   test('All event files have ".md" extension', () => {
     const eventFiles = fs.readdirSync('content/events')
 
-    const areAllValid = eventFiles.every((fileName) => fileName.endsWith('.md') || fileName === '.gitkeep');
+    const areAllValid = eventFiles.every((fileName) => fileName.endsWith('.md'))
 
     expect(areAllValid).toBe(true)
   })
 
   test('All events have correct dates (or TBD)', () => {
-    const eventsDir = 'content/events'
-    const eventFiles = fs.readdirSync(eventsDir)
+    const eventFiles = fs.readdirSync('content/events')
 
-    // Check if the directory is empty
-    if (fs.readdirSync(eventsDir).length === 1) {
-      console.log('Events directory \'s empty. Test is skipped.');
-    } else {
-      const events = eventFiles.map((fileName) => {
-        const slug = fileName.replace('.md', '')
-        const event = getEventBySlug(slug)
-        return event
-      })
+    const events = eventFiles.map((fileName) => {
+      const slug = fileName.replace('.md', '')
+      const event = getEventBySlug(slug)
+      return event
+    })
 
-      events.forEach((event) => {
-        expect(() => parseEvent(event)).not.toThrowError()
-      })
-    }
+    events.forEach((event) => {
+      expect(() => parseEvent(event)).not.toThrowError()
+    })
   })
 })
