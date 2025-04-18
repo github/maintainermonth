897bd42697aea486f947b4c0fbd722d88b1c6e5f
Andrea Liliana Griffiths <andreagriffiths11@github.com>
2025-04-18 14:39:04 +0000

    feat: Improve schedule page layout and event visibility
    - Enhance schedule page layout with multi-column grid display
      - Add responsive grid with up to 4 columns on large screens
      - Improve card layout and spacing utilization
      - Show event descriptions in cards
    
    - Add prominent "Add your activity" button
      - Position button in page header
      - Update button text for clarity
      - Improve button visibility with white background
    
    - Improve visual hierarchy of event information
      - Reorganize event meta information
      - Add subtle hover effects on cards
      - Style date chips consistently
    
    - Add distinctive styling for spoken language chips
      - Add data-spoken-language attribute to identify language indicators
      - Style language chips with purple background (#9C27B0)
      - Add detection for language prefixes and common language names
    
    Files changed:
    - components/events-list/events-list.scss
    - components/events-list/EventsList.jsx
    - components/chip/chip.scss
    - components/chip/Chip.jsx
    - content/commons.json

--- components/events-list/EventsList.jsx
+++ components/events-list/EventsList.jsx
@@ -16,67 +16,71 @@ import Chip from '../chip/Chip'
 const EventsList = ({ events }) => {
   return (
     <section className="events-list">
-      {events.map((event, index) => (
-        <div
-          key={event.slug}
-          className={clsx('events-list__card', {
-            'same-date': index > 0 && event.date === events[index - 1].date,
-          })}
-        >
-          <div className="events-list__date">
-            <Chip label={event.formattedDate.date} />
-          </div>
-
-          <div className="events-list__event">
-            <div className="events-list__chips">
-              <EventTypeChip type={event.type} />
-              {event.language && <Chip label={event.language} />}
-              {event.location && <Chip label={event.location} />}
-            </div>
-            <DateTimeChip
-              startTime={event.formattedDate.startTime}
-              endTime={event.formattedDate.endTime}
-            />
-
-            <div>
-              <a
-                className="events-list__user"
-                href={event.userLink}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {event.userName}
-              </a>
-
-              <Link
-                href={{ pathname: event.link }}
-                className="events-list__link"
-              >
-                {event.title}
-              </Link>
-            </div>
-
-            <div
-              className="events-list__text"
-              dangerouslySetInnerHTML={{
-                __html: md().render(event.content),
-              }}
-            />
-
-            <PlayLink href={event.linkUrl}>
-              {getLiteral(`event-button:${event.type}`)}
-            </PlayLink>
-          </div>
+      <div className="events-list__header">
+        <div className="events-list__header-content">
+          <h1 className="events-list__title">{getLiteral('schedule:title')}</h1>
+          <p className="events-list__subtitle">{getLiteral('schedule:description')}</p>
+          <ButtonLink 
+            href="https://github.com/github/maintainermonth/issues/new?assignees=&labels=&template=add-event.yaml&title=New+Event" 
+            isExternal={true}
+            className="events-list__add-button"
+          >
+            {getLiteral('schedule:add-event')}
+          </ButtonLink>
         </div>
-      ))}
-      <br />
-      <div>
-        <ButtonLink href="https://github.com/github/maintainermonth/issues/new?assignees=&labels=&template=add-to-calendar.yml&title=EVENT_NAME">
-          Add your own activity
-        </ButtonLink>
+      </div>
+
+      <div className="events-list__grid">
+        {events.map((event, index) => (
+          <div
+            key={event.slug}
+            className={clsx('events-list__card', {
+              'same-date': index > 0 && event.date === events[index - 1].date,
+            })}
+          >
+            <div className="events-list__date">
+              <Chip label={event.formattedDate.date} />
+            </div>
+
+            <div className="events-list__event">
+              <div className="events-list__meta">
+                <a
+                  className="events-list__user"
+                  href={event.userLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {event.userName}
+                </a>
+                <Link href={event.link} className="events-list__link">
+                  {event.title}
+                </Link>
+                <DateTimeChip
+                  startTime={event.formattedDate.startTime}
+                  endTime={event.formattedDate.endTime}
+                />
+              </div>
+
+              <div className="events-list__chips">
+                <EventTypeChip type={event.type} />
+                {event.language && <Chip label={event.language} />}
+                {event.location && <Chip label={event.location} />}
+              </div>
+
+              <div className="events-list__info">
+                <p 
+                  className="events-list__text"
+                  dangerouslySetInnerHTML={{
+                    __html: md().render(event.description || ''),
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default EventsList
+export default EventsList;
