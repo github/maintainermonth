ffd293621d146f37a2f390e89c183e886ed42da1
Chris Howarth <cthowarth@live.com>
2025-04-25 23:50:26 +0100

    feat: add tests for event end dates and create test data for Cookie Cutter and Typewind events

---
+++ tests/testData/2025-05-02-Cookie-Cutter.js
@@ -0,0 +1,25 @@
+const cookieCutterEvent = {
+  slug: '2025-05-02-Cookie-Cutter',
+  title: 'Open Source Friday Brasil: Cookie Cutter and FastAPI',
+  metaTitle: 'Open Source Friday Brasil: Cookie Cutter and FastAPI',
+  metaDesc:
+    'Join Arthur Henrique to discuss Cookiecutter template for FastAPI projects using Machine Learning, uv, GitHub Actions, and Pytests.',
+  date: '05/02',
+  endDate: '07/02',
+  UTCStartTime: '16:00',
+  UTCEndTime: '17:00',
+  type: 'stream',
+  location: 'Virtual',
+  language: 'Portuguese',
+  userName: 'GitHub',
+  userLink: 'https://www.youtube.com/github',
+  linkUrl: 'https://www.youtube.com/github',
+  content:
+    '\n' +
+    'Join [Arthur Henrique](https://github.com/arthurhenrique) to discuss Cookiecutter template for FastAPI projects using Machine Learning, uv, GitHub Actions, and Pytests.\n' +
+    '\n' +
+    "Open Source Fridays Brasil stream weekly on GitHub's [Twitch](https://www.twitch.tv/githubbrasil), [YouTube](https://github.com/youtube), and [LinkedIn](https://www.linkedin.com/company/githubbrasil).\n",
+  link: '/schedule/2025-05-02-Cookie-Cutter',
+}
+
+export default cookieCutterEvent
---
+++ tests/testData/2025-05-02-typewind.js
@@ -0,0 +1,24 @@
+const typewindEvent = {
+  slug: '2025-05-02-typewind',
+  title: 'Open Source Friday: Typewind',
+  metaTitle: 'Open Source Friday: Typewind',
+  metaDesc:
+    'Chat with Mokshit Jain about Typewind, a build tool focused on improving web performance.',
+  date: '05/02',
+  UTCStartTime: 'G:00',
+  UTCEndTime: '7:00',
+  type: 'stream',
+  location: 'Virtual',
+  language: 'English',
+  userName: 'GitHub',
+  userLink: 'https://www.twitch.tv/github/schedule',
+  linkUrl: 'https://www.twitch.tv/github/schedule',
+  content:
+    '\n' +
+    'Chat with [Mokshit Jain](https://github.com/Mokshit06) about [Typewind](https://github.com/Mokshit06/typewind), a build tool focused on improving web performance. Explore the journey of creating Typewind and contributions to the open-source build-tools space.\n' +
+    '\n' +
+    "[Open Source Fridays](https://www.youtube.com/playlist?list=PL0lo9MOBetEFmtstItnKlhJJVmMghxc0P) stream weekly on GitHub's [Twitch](https://www.twitch.tv/github), [YouTube](https://www.youtube.com/github), and [LinkedIn](https://www.linkedin.com/company/github).\n",
+  link: '/schedule/2025-05-02-typewind',
+}
+
+export default typewindEvent
