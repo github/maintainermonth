7a3987025ae200cc5d637568c913f55c81fae5b5
Danish Zayan <danishzayan6@gmail.com>
2025-05-14 12:09:06 +0530

    Add EventFilter component and styles for event type selection
    - Implement EventFilter component to allow users to filter events by type.
    - Create eventTypes.js to define available event types with labels.
    - Update EventsList component to integrate EventFilter and manage selected event type.
    - Add styles for EventFilter and update existing styles for EventsList.

--- components/events-list/EventsList.jsx
+++ components/events-list/EventsList.jsx
@@ -1,37 +1,47 @@
-import md from 'markdown-it'
-import clsx from 'clsx'
+import React, { useState } from 'react';
+import md from 'markdown-it';
+import clsx from 'clsx';
+import Link from 'next/link';
 
-import Link from 'next/link'
-
-import ButtonLink from '../button-link/ButtonLink'
-
-import { getLiteral } from '../../common/i18n'
-import * as ROUTES from '../../common/routes'
-
-import DateTimeChip from '../date-time-chip/DateTimeChip'
-import EventTypeChip from '../event-type-chip/EventTypeChip'
-import PlayLink from '../play-link/PlayLink'
-import Chip from '../chip/Chip'
+import ButtonLink from '../button-link/ButtonLink';
+import EventFilter from '../event-filter/EventFilter';
+import eventTypes from '../event-filter/eventTypes';
+import { getLiteral } from '../../common/i18n';
+import DateTimeChip from '../date-time-chip/DateTimeChip';
+import EventTypeChip from '../event-type-chip/EventTypeChip';
+import Chip from '../chip/Chip';
 
 const EventsList = ({ events }) => {
+  const [selectedType, setSelectedType] = useState('all');
+  const filteredEvents = selectedType === 'all'
+    ? events
+    : events.filter((event) => event.type === selectedType);
+
   return (
     <section className="events-list">
       <div className="events-list__header">
         <div className="events-list__header-content">
           <h1 className="events-list__title">{getLiteral('schedule:title')}</h1>
           <p className="events-list__subtitle">{getLiteral('schedule:description')}</p>
-          <ButtonLink 
-            href="https://github.com/github/maintainermonth/issues/new?template=add-to-calendar.yml" 
-            isExternal={true}
-            className="events-list__add-button"
-          >
-            {getLiteral('schedule:add-event')}
-          </ButtonLink>
+          <div className="events-list__controls">
+            <ButtonLink
+              href="https://github.com/github/maintainermonth/issues/new?template=add-to-calendar.yml"
+              isExternal={true}
+              className="events-list__add-button"
+            >
+              {getLiteral('schedule:add-event')}
+            </ButtonLink>
+            <EventFilter
+              selectedType={selectedType}
+              setSelectedType={setSelectedType}
+              eventTypes={eventTypes}
+            />
+          </div>
         </div>
       </div>
 
       <div className="events-list__grid">
-        {events.map((event, index) => (
+        {filteredEvents.map((event, index) => (
           <div
             key={event.slug}
             className={clsx('events-list__card', {
@@ -68,7 +78,7 @@ const EventsList = ({ events }) => {
               </div>
 
               <div className="events-list__info">
-                <p 
+                <p
                   className="events-list__text"
                   dangerouslySetInnerHTML={{
                     __html: md().render(event.description || ''),
