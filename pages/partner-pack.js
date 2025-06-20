4bfe83f7c970da8e59cda05ea927f94a2802ae56
Abby Cabunoc Mayes <abbycabs@github.com>
2025-06-19 22:09:19 -0400

    close partner pack

--- pages/partner-pack.js
+++ pages/partner-pack.js
@@ -47,7 +47,12 @@ export default function PartnerPack({ content, frontmatter, partnerOffers, addit
         </div>
         <div className="partner-pack__content">
           <div dangerouslySetInnerHTML={{ __html: content }}></div>
-          <Offers partnerOffers={partnerOffers} additionalSections={additionalSections} />
+          {/* Offers component commented out until Maintainer Month 2026
+          <Offers partnerOffers={partnerOffers} additionalSections={additionalSections} /> */}
+          <div className="partner-pack__message">
+            <h2>Partner Pack Offers</h2>
+            <p>Thank you for your interest! Maintainer Month 2025 has concluded. Please check back next year for new partner offers and opportunities.</p>
+          </div>
         </div>
       </div>
     </div>
